2f62c682b506cd4147e05f3c8af5562105f0a893
jhansoo <zhanxulam@gmail.com>
2025-02-12 22:57:16 +0800

    Site updated: 2025-02-12 22:57:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/12/TEST/","2025/02/12/hello-world/"];function toRandomPost(){
+var posts=["2025/02/12/hello-world/","2025/02/12/test基于LM Studio本地部署DeepSeek-R1的蒸馏量化模型/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
