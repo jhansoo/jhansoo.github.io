3c841925c26a6b148530462541f02a4fe51f0d7b
jhansoo <zhanxulam@gmail.com>
2025-02-13 15:57:05 +0800

    Site updated: 2025-02-13 15:57:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/12/testLM StudioDeepSeek-R1/","2025/02/12/hello-world/"];function toRandomPost(){
+var posts=["2025/02/12/hello-world/","2025/02/12/testLM StudioDeepSeek-R1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
